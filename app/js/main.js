8c10dfd026430f040f87026144ecc3ecb1a9b793
Paweł Pavlov <57533526+pavlovspace@users.noreply.github.com>
2020-02-03 22:08:45 +0100

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+$('.header__slider').slick({
+    infinite: true,
+    fade:true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+
+});
+  
+});
