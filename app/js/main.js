c3e541c2260fd151605d4477973c877cbe9ae164
Paweł Pavlov <pavlov1space@gmail.com>
2020-02-10 03:39:12 +0100

    fix for slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,23 +1,33 @@
 $(function(){
 
-    $('.header__slider').slick({
-        infinite: true,
-        fade:true,
-        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-        asNavFor: '.slider-dots',
-    });
-    $('.slider-dots').slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        asNavFor: '.header__slider',
-    }) 
+$('.header__slider').slick({
+    infinite: true,
+    fade:true,
+       prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    asNavFor: '.slider-dotshead',
+});
+$('.slider-dotshead').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+    focusOnSelect: true,
+}) 
 
 $('.sport-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    asNavFor: '.slider-map',
+})
+
+$('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.sport-slider',
+    focusOnSelect: true,
 })
 
 });
