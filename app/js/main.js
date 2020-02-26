2e51a115a692d1c9b5285137128f5b6bbf1997e2
Paweł Pavlov <pavlov1space@gmail.com>
2020-02-26 21:51:06 +0100

    implemented about.html

--- app/js/main.js
+++ app/js/main.js
@@ -156,6 +156,30 @@ $('.surfboard-box__circle').on('click', function(){
 //---срабатывание бургера---//
 $('.menu-btn').on('click', function(){
     $('.menu').toggleClass('active');
-})
+});
+//---parallax---//
+$(window).scroll(function(event) {
+  let s=$(this).scrollTop();
+  let w=$(this).outerWidth();
+  let h=$('.content').outerHeight();
+  let h_b=$('.parallax').outerHeight();
+  let p = s/h*100;
+  let p_b = s/h_b*100;
+  let o = 1-1/100*p_b;
 
+  let z_1=1+(w/10000*p_b);
+  $('.parallax__fog').css('transform','scale('+z_1+')');
+  $('.parallax__fog').css('opacity',o);
+
+    let z_2=1+(w/5000000*p);
+  $('.parallax__aqua__1').css('transform','scale('+z_2+')');
+
+  let hr=w/2000*p_b;
+  let z_3=1+(w*0.000005*p_b);
+  $('.parallax__aqua__2').css('transform','translate3d(0'+hr+'px,0,0) scale('+z_3+')');
+
+  let hr_2=w/1500*p_b;
+  let z_4=1+(w*0.00001*p_b);
+  $('.parallax__aqua__3').css('transform','translate3d(0'+hr_2+'px,0,0) scale('+z_4+')');
+});
 });
